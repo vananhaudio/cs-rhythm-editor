abc4dd835b8a9f91a909bc066db81304c66a3a72
Trần Văn Anh <vananhaudio@Trans-MacBook-Pro.local>
2026-05-27 11:53:35 +0700

    fix: khôi phục vite config đơn giản

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@coderline/alphatab'],
-  },
-  worker: {
-    format: 'es',
-  },
-  assetsInclude: ['**/*.mjs'],
 })
